1c82e1cd6b14ebcc4130345bd89418c89fbf743f
Fabian <96096173+PetusoTwo@users.noreply.github.com>
2024-06-03 20:26:19 -0500

    main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Developer: PetusoTwo
 const listaPokemon = document.querySelector("#listaPokemon");
 const botonesHeader = document.querySelectorAll(".btn-header");
 let URL = "https://pokeapi.co/api/v2/pokemon/";
